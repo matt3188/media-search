9dab7a14650235c8a8bd64630b7a01eccebd9eda
Matt Coleman <matt.coleman@protonmail.com>
2017-11-11 17:17:11 +0000

    Created a ShowCard component and imported the data.json

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ShowCard from "./ShowCard";
+import preload from "../data.json";
 
 const Search = () => <h1>Search</h1>;
 
---
+++ js/ShowCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { shape, string } from "prop-types";
+
+const ShowCard = props => (
+);
+
+export default ShowCard;
