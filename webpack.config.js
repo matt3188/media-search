aafe0bfc8e58c36821793c5d18752fd294933b45
Matt Coleman <matt.coleman@protonmail.com>
2017-11-11 12:48:35 +0000

    Added historyApiFallback: true to tell Webpack to let Router handle 404's

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
 const path = require("path");
 
 module.exports = {
-	context: __dirname,
-	entry: "./js/ClientApp.jsx",
-	devtool: "cheap-eval-source-map",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js"
-	},
-	resolve: {
-		extensions: [".js", ".jsx", ".json"]
-	},
-	stats: {
-		colors: true,
-		reasons: true,
-		chunks: true
-	},
-	devServer: {
-		publicPath: "/public/"
-	},
-	module: {
-		rules: [
-			{
-				enforce: "pre",
-				test: /\.jsx?$/,
-				loader: "eslint-loader",
-				exclude: /node_moudles/
-			},
-			{
-				test: /\.jsx?$/,
-				loader: "babel-loader"
-			}
-		]
-	}
+  context: __dirname,
+  entry: "./js/ClientApp.jsx",
+  devtool: "cheap-eval-source-map",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  devServer: {
+    publicPath: "/public/",
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        loader: "eslint-loader",
+        exclude: /node_moudles/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
