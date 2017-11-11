455387611e9572d54252ae51678afe1e406df7d0
Matt Coleman <matt.coleman@protonmail.com>
2017-11-11 17:18:42 +0000

    Passed in the data with props and displayed everything in the ShowCard

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => <h1>Search</h1>;
+const Search = () => <div className="search">{preload.shows.map(show => <ShowCard show={show} />)}</div>;
 
 export default Search;
