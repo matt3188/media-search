2470d1ddbe47c6ac5295bb3fe12f47a4c1f51ca0
Matt Coleman <matt.coleman@protonmail.com>
2017-11-11 20:49:49 +0000

    Converted Search to a Class with state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-  <div className="search">
-    <div>{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div className="search">
+        <div>{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+      </div>
+    );
+  }
+}
 
 export default Search;
