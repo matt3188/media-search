6c0b08487417dbc2dfc67588d412b2465b23fa83
Matt Coleman <matt.coleman@protonmail.com>
2017-11-12 14:12:47 +0000

    Swapped out renderer for enzyme and wrapped it in describe
    Updated snapshot

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import Search from "../Search";
 
-test("Search renders correctly", () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+const component = shallow(<Search />);
+
+describe("Search", () => {
+  it("renders correctly", () => {
+    expect(component).toMatchSnapshot();
+  });
+  });
 });
