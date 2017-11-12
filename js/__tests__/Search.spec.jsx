3139581a9e1da086ab2ea7c80b881de6c9c3c228
Matt Coleman <matt.coleman@protonmail.com>
2017-11-12 14:12:54 +0000

    Added two more tests

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
+import preload from "../../data.json";
 import Search from "../Search";
+import ShowCard from "../ShowCard";
 
 const component = shallow(<Search />);
 
@@ -8,5 +10,18 @@ describe("Search", () => {
   it("renders correctly", () => {
     expect(component).toMatchSnapshot();
   });
+
+  it("should render correct amount of shows", () => {
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+  });
+
+  it("should render correct amount of shows based on search term", () => {
+    const searchWord = "black";
+    component.find("input").simulate("change", { target: { value: searchWord } });
+
+    const showCount = preload.shows.filter(
+      show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+    ).length;
+    expect(component.find(ShowCard).length).toEqual(showCount);
   });
 });
